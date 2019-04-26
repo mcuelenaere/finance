d49df88dfdb42693cc6822f759b3330e5ffb0328
Maurus Cuelenaere <mcuelenaere@gmail.com>
2019-04-26 18:17:45 +0200

    Generate weekly plot as well + limit transactions in time for weekly/monthly plots

--- generate_graphs.py
+++ generate_graphs.py
@@ -1,18 +1,24 @@
 import pygal
 import sys
 from collections import defaultdict
+from datetime import timedelta
 from itertools import groupby
 from finance.config import parse_config
 from pygal.style import DefaultStyle
+
+
+def sort_and_group(data, key):
+    data = sorted(data, key=key)
+    return groupby(data, key)
 
 
 def render_stacked_chart(transactions, time_format: str, title: str):
     # group by time unit
     x_labels = []
     series = defaultdict(dict)
-    for time_unit, batch in groupby(transactions, key=lambda t: t.timestamp.strftime(time_format)):
+    for time_unit, batch in sort_and_group(transactions, key=lambda t: t.timestamp.strftime(time_format)):
         x_labels.append(time_unit)
-        for category, batch in groupby(batch, key=lambda t: categorizer.categorize(t)):
+        for category, batch in sort_and_group(batch, key=lambda t: categorizer.categorize(t)):
             series[category][time_unit] = sum(t.amount for t in batch)
 
     # generate graph
@@ -35,6 +41,20 @@ if __name__ == "__main__":
     config = parse_config(sys.argv[1])
     categorizer = config.categorizer()
     transactions = sorted(config.transactions(), key=lambda t: (t.timestamp, t.source.number))
+    last_transaction_time = max(t.timestamp for t in transactions)
 
-    render_stacked_chart(transactions, '%Y-%m', title='Transactions per month').render_to_file('per_month.svg')
+    six_months_ago = last_transaction_time - timedelta(weeks=4 * 6)
+    render_stacked_chart(
+        [t for t in transactions if t.timestamp > six_months_ago],
+        time_format='%Y-%W',
+        title='Transactions per week'
+    ).render_to_file('per_week.svg')
+
+    two_years_ago = last_transaction_time - timedelta(weeks=4 * 12 * 2)
+    render_stacked_chart(
+        [t for t in transactions if t.timestamp > two_years_ago],
+        time_format='%Y-%m',
+        title='Transactions per month'
+    ).render_to_file('per_month.svg')
+
     render_stacked_chart(transactions, '%Y', title='Transactions per year').render_to_file('per_year.svg')
